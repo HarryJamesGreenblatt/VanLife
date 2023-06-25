85130906629215527a34761ee7ccb07964e106e5
HarryJamesGreenblatt <HarryJamesGreenblatt@ServiceCentricSolutions.com>
2023-06-24 22:50:39 -0700

    modified:   package-lock.json modified:   package.json modified:   src/App.jsx new file:   src/pages/Vans.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
+import About from "./pages/About.jsx"; 
+import Vans from "./pages/Vans.jsx"; 
 
 function App() {
   return (
@@ -9,11 +10,13 @@ function App() {
         <Link className="link site-logo"  to="/">#VANLIFE</Link>
         <nav>
           <Link className="link" to="/about">About</Link>
+          <Link className="link" to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Vans.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+export default function Vans() {
+    
+    React.useEffect(
+        () => {
+            // promise chaining method
+            fetch("/api/vans")
+                .then(res => console.log(res.json()))
+                // .then(data => data.vans.map(
+                //     van => console.log(van)
+                // ));
+            
+            // async await method
+            // async function getVans(){
+            //     const res = await fetch("api/vans");
+            //     const data = await res.json();
+            //     data.vans.map(
+            //         van => console.log(van)
+            //     );
+            // }
+            // getVans();
+        },
+        []
+    )
+    
+    return (
+        <h1>Vans page goes here 🚐</h1>
+    )
+}
